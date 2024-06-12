daf0d763bc9d5069bac61ec8c236b97f90671aea
ayushkarangutkar <ayushkarangutkar26@gmail.com>
2024-06-12 17:39:59 +0530

    Create fullDiamond.cpp

---
+++ day1/pattern/fullDiamond.cpp
@@ -0,0 +1,42 @@
+
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int n,i,j;
+    n=5;
+        for( int i=1;i<=5;i++)
+    {
+        for( int j=1;j<=5;j++)
+        {
+            if(i<=n-j)
+            {
+                cout<<" ";
+            }
+            else
+            {
+                cout<<" * ";
+            }
+        }
+        cout<<endl;
+    }
+   
+
+    for( int i=1;i<=5;i++)
+    {
+        for( int j=1;j<=5;j++)
+        {
+            if(j>=i)
+            {
+                cout<<" * ";
+            }
+            else
+            {
+                cout<<"  ";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+    }
+    

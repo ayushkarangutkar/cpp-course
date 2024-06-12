efc5dea5898583a71603884d2adf840963965bef
ayushkarangutkar <ayushkarangutkar26@gmail.com>
2024-06-12 11:23:06 +0530

    Create alphabetPyramid.cpp

---
+++ day1/pattern/alphabetPyramid.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i, j;
+    char k;
+    k='a';
+    
+
+    for( int i=1;i<=5;i++)
+    { 
+        for( int j=1;j<=i;j++)
+        {
+            cout<<k;
+            
+            
+        }
+        k++;
+        cout<<endl;
+    }
+    return 0;
+}
+

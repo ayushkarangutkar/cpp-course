61d7254574a322ed9c75644c608593ef79a174b1
ayushkarangutkar <ayushkarangutkar26@gmail.com>
2024-06-16 11:47:19 +0530

    1

--- day1/pattern/rotatedNumberPyramid.cpp
+++ day1/pattern/rotatedNumberPyramid.cpp
@@ -4,9 +4,12 @@ using namespace std;
 int main() {
 
     int n=5;
-    for(int i=1; i<=n; i++){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n-i; j++){
+            cout<<" ";
+        }
         for(int j=i; j<=2*i-1; j++){
-            cout<<j<<" ";
+            cout<<j;
         }
         cout<<endl;
     }

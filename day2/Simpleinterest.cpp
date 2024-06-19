70bb9c3c92e0db62a4df14e50234f4f555e41cd7
ayushkarangutkar <ayushkarangutkar26@gmail.com>
2024-06-19 10:48:52 +0530

    Create Simpleinterest.cpp

---
+++ day2/Simpleinterest.cpp
@@ -0,0 +1,12 @@
+//C++ Program To Find Simple Interest
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    float P=5,T=3,R=7;
+    float SI=(P*T*R/100);
+    cout<<"the simple interest is "<<SI;
+
+    return 0;
+}

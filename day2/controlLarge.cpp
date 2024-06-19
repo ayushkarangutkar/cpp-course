e3437d7d0696d0f42f04cd4f3afff07ee35fc1b3
ayushkarangutkar <ayushkarangutkar26@gmail.com>
2024-06-19 11:23:15 +0530

    Update controlLarge.cpp

--- day2/controlLarge.cpp
+++ day2/controlLarge.cpp
@@ -4,16 +4,17 @@ using namespace std;
 int main()
 {
     int num1,num2,num3;
-    num1=23,num2=34,num3=45;
-    if(num1>>num2 && num1>>num3)
+    cout<<"\nEnter value of three number";
+    cin>>num1>>num2>>num3;
+    if(num1>num2 && num1>num3)
     {
         cout<<"\n The largest num is "<<num1;
     }
-    else if( num2>>num1 && num2>>num3)
+    else if( num2>num1 && num2>num3)
     {
        cout<<"\n The largest num is "<<num2;
     }
-    else if( num3>>num2 && num3>>num2)
+    else if( num3>num2 && num3>num2)
     {
             cout<<"\n The largest num is "<<num3;
     }

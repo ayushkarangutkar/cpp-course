1ab7576157f9f0bfa0beb5d9b26ed5881e51a873
ayushkarangutkar <ayushkarangutkar26@gmail.com>
2024-06-26 10:38:12 +0530

    Create GetAndSetdetails.cpp

---
+++ day3/GetAndSetdetails.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+class navjeevan
+{
+    private:
+    float marks;
+    public:
+    string name;
+    int age;
+    string branch;
+    
+    float getter()
+    {
+        return marks;
+    }
+    void setter(float ear)
+    {
+        marks=ear;
+    }
+};
+    int main()
+    {
+        navjeevan n1;
+        n1.name="aryan";
+        n1.age=17;
+        n1.branch="CO";
+        
+        n1.setter(82.60);
+        float ear=n1.getter();
+        cout<<"NAME"<<"-"<<n1.name<<endl;
+        cout<<"AGE"<<"-"<<n1.age<<endl;
+        cout<<"BRANCH"<<"-"<<n1.branch<<endl;
+        cout<<"MARKS"<<"-"<<ear<<endl;
+
+    return 0;
+}

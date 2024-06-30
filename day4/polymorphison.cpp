78a012d36b99d22f36f65922100b5c4a37037bce
ayushkarangutkar <ayushkarangutkar26@gmail.com>
2024-06-30 10:33:42 +0530

    Update polymorphison.cpp

--- day4/polymorphison.cpp
+++ day4/polymorphison.cpp
@@ -9,16 +9,32 @@ class student
         string name;
         int id;
         float perc;
+        
+       // student()
+           // cout<<"the student is my"<<endl;
+       // student(int a)
+           // cout<<"constucter called is my";
+        
+        void student()
+        {
+            cout<<"this people is my"<<endl;
+        }
+        void student(int b)
+        {
+            cout<<"this is yours";
+        }
+
 };
 
 int main()
 {
+    //student s1(3);
     student s1;
     s1.age=23;
     s1.name="soham";
     s1.id=3456;
     s1.perc=98.98;
-
+    student(2);
     return 0;
     
 }

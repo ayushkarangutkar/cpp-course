9e55fb6abf29eb4e2785bd09d2dfc180e9309f10
ayushkarangutkar <ayushkarangutkar26@gmail.com>
2024-07-04 19:20:30 +0530

    Create calculator3.cpp

---
+++ day4/assignment/calculator3.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+class calculator
+{
+    public:
+    double add(double a,double b)
+      {
+        double num1=a;
+        double num2=b;
+        double add;
+       add=num1+num2;
+       cout<<"ADD -"<<add<<endl;
+      // return(add);
+      }
+    double sub(double a,double b)
+      {
+        double num1=a;
+        double num2=b;
+        double sub;
+        sub=num1-num2;
+        //return(sub);
+        cout<<"SUB -"<<sub<<endl;
+      }
+    double mul(double a,double b)
+      {
+        double num1=a;
+        double num2=b;
+         double mul;
+        mul=num1*num2;
+       // return(mul);
+       cout<<"MUL-"<<mul<<endl;
+      }
+    double div(double a,double b)
+      { 
+        double num1=a;
+        double num2=b;
+        double div;
+        div=num1/num2;
+        // return(div);
+       cout<<"DIV -"<<div<<endl;
+      }
+};
+  int main()
+  {
+      calculator c1;
+      double a=5.2;
+      double b=3.4;
+      double result;
+      c1.add(a,b);
+      c1.sub(a,b);
+      c1.mul(a,b);
+      c1.div(a,b);
+      return 0;
+
+
+  }

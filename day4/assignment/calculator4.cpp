6c420bb99055bb5b3ffb1f9232a6ea77f99af567
ayushkarangutkar <ayushkarangutkar26@gmail.com>
2024-07-05 20:09:41 +0530

    Create calculator4.cpp

---
+++ day4/assignment/calculator4.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+class calculator
+{
+    double add(double a,double b)
+    {
+        double add;
+        add=a+b;
+        return (add);
+    }
+}; 
+int main()
+{
+    calculator c1;
+    double a,b,k;
+    cout<<"Enter the two value"<<endl;
+    cin>>a>>b;
+    k=add(a,b);
+    cout<<endl<
+}

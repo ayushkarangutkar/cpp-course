9376fa3e7458ee8d6257b40ccaf60dc3829f3eaa
ayushkarangutkar <ayushkarangutkar26@gmail.com>
2024-07-09 17:57:24 +0530

    Update Math1.cpp

--- day5/Math1.cpp
+++ day5/Math1.cpp
@@ -21,15 +21,26 @@ class sciencecal
        res= sin(num2*pi/180);
        return(res);
     }
+    double can(double e)
+    {
+       double num3=e;
+       double res;
+       float pi=3.14159265;
+       res= sin(num3*pi/180);
+       return(res);
+    }
 };
 int main()
 {
     sciencecal s1;
     double v=30.7;
     double a=40.5;
+    double e=20.4;
     double cosed=s1.fun(v);
     double sined=s1.run(a);
+    double taned=s1.can(e);
     cout<<"The cosine of "<<v<<" degree is "<<cosed<<endl;
-     cout<<"The sine of "<<a<<" degree is "<<sined<<endl;
+    cout<<"The sine of "<<a<<" degree is "<<sined<<endl;
+    cout<<"The tan of "<<e<<" degree is "<<taned<<endl;
     return 0;
 }

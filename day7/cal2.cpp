c9d91c7d3488e0e6f7b384913316384bfff2d177
ayushkarangutkar <ayushkarangutkar26@gmail.com>
2024-08-12 19:35:28 +0530

    Create cal2.cpp

---
+++ day7/cal2.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+#include<math.h>
+#include<string>
+using namespace std;
+
+class calculator
+{
+    public:
+    void cal()
+    {
+        int num1,num2,result;
+        char ch;
+        cout<<"enter the operators - "<<endl;
+        cout<<'+'<<"  "<<'-'<<"  "<<'*'<<"  "<<'/'<<"   "<<'%'<<endl;
+        cout<<"\n";
+        
+        cout<<"enter the number"<<endl;
+        cout<<"num 1 = ";
+        cin>>num1;
+        cout<<"opertor :- ";
+        cin>>ch;
+        cout<<"num 2 = ";
+        cin>>num2;
+        cout<<"\n";
+        
+        if(ch=='+')
+        {
+            cout<<"result :- "<<endl;
+            result=num1+num2;
+            cout<<num1<<" "<<'+'<<" "<<num2<<" = "<<result<<endl;
+        }
+        else if(ch=='-')
+        {
+            cout<<"result : - "<<endl;
+            result=num1-num2;
+            cout<<num1<<" "<<'-'<<" "<<num2<<" = "<<result<<endl;
+
+        }
+        else if(ch=='*')
+        {
+            cout<<"result :-  "<<endl;
+            result=num1*num2;
+            cout<<num1<<" "<<'*'<<" "<<num2<<" = "<<result<<endl;
+        }
+        else if(ch=='/')
+        {
+            cout<<"result :- "<<endl;
+            result=num1/num2;
+            cout<<num1<<" "<<'/'<<" "<<num2<<" = "<<result<<endl;
+        }
+        else if(ch == '%')
+        {
+            cout<<"result :- "<<endl;
+            result=num1%num2;
+            cout<<num1<<" % "<<num2<<" = "<<result
+            <<endl;
+        }
+    }
+};  
+    int main()
+    { 
+        char x;
+        do
+        {
+        calculator c1;
+        c1.cal();
+        cout<<" \n enter N to exit or Y to continue :- ";
+        cin>>(x);
+        }
+        while(x !='n');
+    }
